6604bd8a06fab1b81641c604043c4fc2c549e1f8
Mohan Kumar <mkobike123@gmail.com>
2023-03-09 15:53:55 +0800

    commit project

---
+++ src/views/Home.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
+    <div
+      class="
+        z-20
+        flex
+        justify-center
+        relative
+        bg-hero-pattern bg-cover
+        px-4
+        pt-8
+        pb-32
+      "
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            v-model="queryIp"
+            class="
+              flex-1
+              py-3
+              px-2
+              rounded-tl-md rounded-bl-md
+              focus:outline-none
+            "
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your IP info"
+          />
+          <i
+            @click="getIpInfo"
+            class="
+              cursor-pointer
+              bg-black
+              text-white
+              px-4
+              rounded-tr-md rounded-br-md
+              flex
+              items-center
+              fa-solid fa-chevron-right
+            "
+          ></i>
+        </div>
+      </div>
+      <!-- IP Info -->
+      <IPInfo v-if="ipInfo" v-bind:ipInfo="ipInfo" />
+    </div>
+    <!-- Map -->
+    <div id="mapid" class="h-full z-10"></div>
+  </div>
+</template>
+
+<script>
+import IPInfo from "../components/IPInfo.vue";
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+import axios from "axios";
+export default {
+  name: "Home",
+  components: { IPInfo },
+  setup() {
+    let mymap;
+    const queryIp = ref("");
+    const ipInfo = ref(null);
+    onMounted(() => {
+      //when component loaded
+      mymap = leaflet.map("mapid").setView([51.505, -0.09], 13);
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmVya2luZHV6IiwiYSI6ImNreXVoZGNqejFncHgyd3Rna3Fmbjljb2wifQ.qlugHadb6y6VXZQda9rjKg",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1IjoiYmVya2luZHV6IiwiYSI6ImNreXVoZGNqejFncHgyd3Rna3Fmbjljb2wifQ.qlugHadb6y6VXZQda9rjKg",
+          }
+        )
+        .addTo(mymap);
+    });
+
+    const getIpInfo = async () => {
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_oAm8fY42BgliJWY78p3w9dg08uYcL&ipAddress=${queryIp.value}`
+        );
+        const result = data.data;
+        ipInfo.value = {
+          address: result.ip,
+          state: result.location.region,
+          timezone: result.location.timezone,
+          isp: result.isp,
+          lat: result.location.lat,
+          lng: result.location.lng,
+        };
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
+        mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+      } catch (error) {
+        alert("Invalid IP Address");
+      }
+    };
+
+    return { queryIp, ipInfo, getIpInfo };
+  },
+};
+</script>
